e1aee4c4164525cfd67ad129ae316877b9711605
Anthony <ringeisen.anthony.dev@gmail.com>
2021-02-18 16:15:03 +0100

    root

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import data from './data';
 import './App.css';
 import List from './components/List';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projets from './pages/Projets';
+import Contact from './pages/Contact';
 
 function App() {
   const [student, setStudent] = useState(data);
   return (
-   <main>
-     <Navbar/>
-     <section className="container">
-        <h2>Attendance List for {student.length} students</h2>
-        <List student={student}/>
-        <div id="list"></div>
-        <button onClick = {() => setStudent([])}>Clear list</button>
-     </section>
-   </main>
+    <Router>
+      <main>
+        <Navbar/>
+        <Switch>
+          <Route path="/" component={Home}>
+            <section className="container">
+              <h2>Attendance List for {student.length} students</h2>
+              <List student={student}/>
+              <div id="list"></div>
+              <button onClick = {() => setStudent([])}>Clear list</button>
+            </section>
+          </Route>
+          <Route path="/projets" component={Projets}/>
+          <Route path="/contact" component={Contact}/>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div>
             <nav className="menu">
                 <ul className="navbar">
-                    <li><a href="">HOME</a></li>
+                    <li><link><a href="">HOME</a></link></li>
                     <li><a href="">PROJETS</a></li>
                     <li><a href="">CONTACT</a></li>
                 </ul>
