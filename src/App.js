0da10b6987d47f5239529ec3cd08c98d80738b48
Anthony <ringeisen.anthony.dev@gmail.com>
2021-02-18 13:25:22 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import data from './data';
 import './App.css';
 import List from './components/List';
+import Navbar from './components/Navbar';
 
 function App() {
   const [student, setStudent] = useState(data);
   return (
    <main>
      <section className="container">
+        <Navbar/>
         <h2>Attendance List for {student.length} students</h2>
         <List student={student}/>
         <div id="list"></div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="menu">
+                <ul className="navbar">
+                    <li><a href="">HOME</a></li>
+                    <li><a href="">PROJETS</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
